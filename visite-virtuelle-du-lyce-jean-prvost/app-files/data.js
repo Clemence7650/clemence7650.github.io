ee366a1bc463a381b3c11e49fd54f70bdd027b6f
Clemence7650 <clemlabeylie@gmail.com>
2025-02-04 09:47:45 +0100

    update

---
+++ visite-virtuelle-du-lyce-jean-prvost/app-files/data.js
@@ -0,0 +1,672 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-devant-bat-e",
+      "name": "devant bat E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3329393207122173,
+          "pitch": 0.010888968006483424,
+          "rotation": 0,
+          "target": "3-sculpture"
+        },
+        {
+          "yaw": -1.615872471711043,
+          "pitch": 0.022413550502012214,
+          "rotation": 0,
+          "target": "1-gauche-du-bat-e"
+        },
+        {
+          "yaw": 2.6722425982675677,
+          "pitch": -0.12146917045954453,
+          "rotation": 0,
+          "target": "9-bat-a"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-gauche-du-bat-e",
+      "name": "gauche du bat E",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0592684462593702,
+          "pitch": 0.047140551876820425,
+          "rotation": 0,
+          "target": "0-devant-bat-e"
+        },
+        {
+          "yaw": 0.5034545756881457,
+          "pitch": 0.002324709339955433,
+          "rotation": 0,
+          "target": "2-rsidences"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-rsidences",
+      "name": "résidences",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0016281199717554,
+          "pitch": 0.06312090935671755,
+          "rotation": 0,
+          "target": "1-gauche-du-bat-e"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-sculpture",
+      "name": "sculpture",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.636853572502714,
+          "pitch": 0.05450533615149311,
+          "rotation": 0,
+          "target": "0-devant-bat-e"
+        },
+        {
+          "yaw": -2.1789072139005228,
+          "pitch": 0.10485833590559146,
+          "rotation": 0,
+          "target": "7-devant-mdl"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-devant-bat-d",
+      "name": "devant bat D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8536620741839487,
+          "pitch": -0.04911464449384795,
+          "rotation": 0,
+          "target": "5-hall-d"
+        },
+        {
+          "yaw": -2.832836996267295,
+          "pitch": 0.11588388940075056,
+          "rotation": 0,
+          "target": "7-devant-mdl"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hall-d",
+      "name": "hall D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2660468743839903,
+          "pitch": 0.1077578795007419,
+          "rotation": 0,
+          "target": "4-devant-bat-d"
+        },
+        {
+          "yaw": -1.6944302731933618,
+          "pitch": 0.08436944703512061,
+          "rotation": 0,
+          "target": "11-tude_bat_d"
+        },
+        {
+          "yaw": -1.3755894555005845,
+          "pitch": 0.27266777798328334,
+          "rotation": 0,
+          "target": "12-bat_d_3_"
+        },
+        {
+          "yaw": 1.523568424936533,
+          "pitch": 0.07655334434219796,
+          "rotation": 0,
+          "target": "10-derrire-bat-c-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0704356793446514,
+          "pitch": -0.03801961501054052,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Télé informative</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-entre",
+      "name": "Entrée",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.06374245963805514,
+        "pitch": -0.009716907074626135,
+        "fov": 1.2739945160662665
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0896376650371877,
+          "pitch": 0.15194124358482242,
+          "rotation": 0,
+          "target": "9-bat-a"
+        },
+        {
+          "yaw": 0.7676861804568418,
+          "pitch": 0.019199012505126234,
+          "rotation": 0,
+          "target": "10-derrire-bat-c-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.317486558893254,
+          "pitch": 0.008174112404535805,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Sortie</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-devant-mdl",
+      "name": "Devant MDL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3747912311271975,
+          "pitch": 0.07532703706630173,
+          "rotation": 0,
+          "target": "4-devant-bat-d"
+        },
+        {
+          "yaw": 0.6105391293338762,
+          "pitch": 0.09644121556489438,
+          "rotation": 0,
+          "target": "8-intrieur-mdl"
+        },
+        {
+          "yaw": -2.8889314864771567,
+          "pitch": 0.07383441377822209,
+          "rotation": 0,
+          "target": "3-sculpture"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-intrieur-mdl",
+      "name": "Intérieur MDL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.512345612639752,
+          "pitch": 0.018769386229877227,
+          "rotation": 0,
+          "target": "7-devant-mdl"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5366776672795961,
+          "pitch": -0.006471836091234806,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Babyfoot</font></font>",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.1158697436384735,
+          "pitch": 0.018209013642653815,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Billard</font></font>",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "9-bat-a",
+      "name": "Bat A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.007231092942135,
+          "pitch": 0.0998734641908321,
+          "rotation": 0,
+          "target": "0-devant-bat-e"
+        },
+        {
+          "yaw": -0.09506550386566559,
+          "pitch": 0.04698602157117193,
+          "rotation": 0,
+          "target": "6-entre"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-derrire-bat-c-",
+      "name": "derrière bat C ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.714603155186558,
+          "pitch": 0.007309686052090925,
+          "rotation": 0,
+          "target": "4-devant-bat-d"
+        },
+        {
+          "yaw": -2.9373421989873236,
+          "pitch": -0.01616745043928347,
+          "rotation": 0,
+          "target": "5-hall-d"
+        },
+        {
+          "yaw": 0.2951781629599566,
+          "pitch": 0.09088497998993716,
+          "rotation": 0,
+          "target": "6-entre"
+        },
+        {
+          "yaw": 1.8635588777462289,
+          "pitch": -0.04504079555955798,
+          "rotation": 0,
+          "target": "13-cantine_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-tude_bat_d",
+      "name": "Étude_bat_D",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27523284387745406,
+          "pitch": 0.0543007140080487,
+          "rotation": 0,
+          "target": "12-bat_d_3_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-bat_d_3_",
+      "name": "Bat_d_3_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4474525641075093,
+          "pitch": 0.06572779337543544,
+          "rotation": 0,
+          "target": "11-tude_bat_d"
+        },
+        {
+          "yaw": 1.3596469116768368,
+          "pitch": 0.1874844207609634,
+          "rotation": 0,
+          "target": "5-hall-d"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-cantine_",
+      "name": "Cantine_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.851009556397102,
+          "pitch": 0.09374694049252597,
+          "rotation": 0,
+          "target": "5-hall-d"
+        },
+        {
+          "yaw": -1.295511842567258,
+          "pitch": 0.029293384305413284,
+          "rotation": 0,
+          "target": "10-derrire-bat-c-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Visite virtuelle du lycée Jean Prévost",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
